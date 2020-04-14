edbb1ac0646c49689349a1567c8e998db313f15c
allan-viveiros <viveirosallan@gmail.com>
2020-04-13 21:46:28 -0400

    fisrt commit. beta

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+//This file will connect to the api and recovery data
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+    let changeableURL = url;
+
+    if(country) {
+        changeableURL = `${url}/countries/${country}`;
+    }
+
+    try {
+        const {data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableURL);
+
+        //When the object is destructured, I can just use with the same name  
+        return {confirmed, recovered, deaths, lastUpdate}
+
+    }
+    catch(error) {
+
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+
+        //console.log(response);
+        return countries.map((country) => country.name);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
